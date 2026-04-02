2cd4a5965661fcfe2ec251cf5145094bf51339a3
ibrhimdd <ibrahimalkhooly55@gmail.com>
2026-04-02 03:54:55 +0200

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -59,4 +59,4 @@ export const GET_COLLEGE_INFO_TOOL = {
   },
 };
 
-export const MODEL_NAME = "gemini-3.1-flash-live-preview";
+export const MODEL_NAME = "gemini-2.5-flash-native-audio-preview-09-2025";
