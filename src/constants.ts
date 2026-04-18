dd5ba0facdec9a38a92c8254110fa3f7d5896471
ibrhimdd <ibrahimalkhooly55@gmail.com>
2026-04-18 10:39:03 +0200

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -90,6 +90,5 @@ export const SAVE_QUESTION_ANSWER_TOOL = {
   },
 };
 
-export const MODEL_NAME = "gemini-1.5-flash-8b";
-
+export const MODEL_NAME = "gemini-2.5-flash-native-audio-preview-09-2025";
 
